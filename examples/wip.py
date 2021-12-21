8ddafe0e763ca37d678354484424381832186e74
Lunarmagpie <Bambolambo0@gmail.com>
2021-12-21 06:46:14 -0500

    :memo: created example

---
+++ examples/wip.py
@@ -0,0 +1,43 @@
+from asyncio import create_task
+
+from songbird import Driver
+from pincer.client import Client
+from pincer.core.dispatch import GatewayDispatch
+from pincer.objects import VoiceServerUpdateEvent, VoiceState
+
+class Bot(Client):
+    @Client.event
+    async def on_ready(self):
+        await self.send(str(GatewayDispatch(
+            4,
+            {
+                "guild_id": 750862883075915826,
+                "channel_id": 919040848237330462,
+                "self_mute": False,
+                "self_deaf": False
+            }
+        )))
+
+        wait_for_state = create_task(self.wait_for(
+            "on_voice_state_update", lambda state: state.user_id == self.bot.id)
+        )
+        wait_for_server = create_task(self.wait_for("on_voice_server_update"))
+
+        state: VoiceState = await wait_for_state
+        server: VoiceServerUpdateEvent = await wait_for_server
+
+        driver = Driver()
+        await driver.make_driver()
+        await driver.connect(
+            token=server.token,
+            endpoint=server.endpoint,
+            session_id=state.session_id,
+            guild_id=server.guild_id,
+            channel_id=state.channel_id,
+            user_id=state.user_id
+        )
+
+        print(driver)
+
+
+Bot("YOUR TOKEN HERE").run()
