0857efee19f6b4b864b4f532553bf2287ee517e9
Lunarmagpie <Bambolambo0@gmail.com>
2021-12-25 00:21:03 -0500

    :bug: make Hikari example better

--- examples/hikari.py
+++ examples/hikari.py
@@ -1,8 +1,9 @@
+from asyncio import sleep
+
 import hikari
 from songbird import Source
 from songbird.hikari import Voicebox
 
-from asyncio import sleep
 
 bot = hikari.GatewayBot("...")
 
@@ -16,20 +17,13 @@ async def ping(event: hikari.ShardReadyEvent) -> None:
         voice_connection_type=Voicebox
     )
 
-    handle = await voice.play_source(Source.ytdl("https://www.youtube.com/watch?v=r25MAkzkTF4"))
+    track_handle = await voice.play_source(Source.ytdl("https://www.youtube.com/watch?v=r25MAkzkTF4"))
 
-    sleep(5)
-
-    # Pausing is synchronous and non-blocking.
-    handle.pause()
-
-    sleep(5)
-
-    handle.play()
-
-    sleep(5)
-
-    # Voicebox is garbage collected here so It stops playing
+    await sleep(5)
+    # Doesn't need to be awaited!
+    track_handle.pause()
+    await sleep(5)
+    track_handle.play()
 
 
 bot.run()
